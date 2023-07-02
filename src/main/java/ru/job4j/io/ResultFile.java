b180e6bde660b25978fd496be066d67e67682f35
Vetoshkin-Oleg <voa2014@gmail.com>
2023-07-02 21:03:58 +0500

    2.0. FileOutputStream [#252488  [#252488 #427098]]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,11 +5,25 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
-            int[][] array = Matrix.multiple(7);
-            String s = Matrix.arrayToString(array);
+            StringBuilder result = new StringBuilder();
+            result.append("1 * 2 = 2");
+            result.append(System.lineSeparator());
+            result.append("1 * 3 = 3");
+            result.append(System.lineSeparator());
+            result.append("1 * 4 = 4");
+            result.append(System.lineSeparator());
+            result.append("1 * 5 = 5");
+            result.append(System.lineSeparator());
+            result.append("1 * 6 = 6");
+            result.append(System.lineSeparator());
+            result.append("1 * 7 = 7");
+            result.append(System.lineSeparator());
+            result.append("1 * 8 = 8");
+            result.append(System.lineSeparator());
+            result.append("1 * 9 = 9");
+            result.append(System.lineSeparator());
 
-            out.write(s.getBytes());
-            out.write(System.lineSeparator().getBytes());
+            out.write(result.toString().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
