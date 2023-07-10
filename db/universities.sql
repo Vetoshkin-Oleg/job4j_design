f66791afaaf6b5c2770e26d0927fa469afba2f70
Vetoshkin-Oleg <voa2014@gmail.com>
2023-07-10 20:35:25 +0500

    0. Select [#6861  [#6861]]

---
+++ db/universities.sql
@@ -0,0 +1,15 @@
+create table universities (
+    id   serial primary key,
+    name text
+);
+
+insert into universities(name)
+values ('U1');
+insert into universities(name)
+values ('U2');
+insert into universities(name)
+values ('U3');
+insert into universities(name)
+values ('U4');
+insert into universities(name)
+values ('U5');
