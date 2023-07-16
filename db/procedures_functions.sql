20b4cc0c27688641fa76dbcf0c9942754d23df58
Vetoshkin-Oleg <voa2014@gmail.com>
2023-07-16 22:56:19 +0500

    3. Хранимые процедуры и функции [#504804  [#504804]]

--- db/procedures_functions.sql
+++ db/procedures_functions.sql
@@ -1,4 +1,4 @@
-create table products (
+CREATE table products (
     id serial primary key,
     name varchar(50),
     producer varchar(50),
@@ -6,55 +6,31 @@ create table products (
     price integer
 );
 
-create or replace procedure insert_data(i_name varchar, prod varchar, i_count integer, i_price integer)
+INSERT INTO products (name, producer, count, price)
+    VALUES ('product_2', 'producer_2', 15, 32);
+INSERT INTO products (name, producer, count, price)
+    VALUES ('product_3', 'producer_3', 20, 40);
+INSERT INTO products (name, producer, count, price)
+    VALUES ('product_4', 'producer_4', 30, 50);
+
+create or replace procedure delete_data()
 language 'plpgsql'
 as $$
     BEGIN
-    insert into products (name, producer, count, price)
-    values (i_name, prod, i_count, i_price);
+    DELETE FROM products WHERE price < 41;
     END
 $$;
 
-create or replace procedure update_data(u_count integer, tax float, u_id integer)
-language 'plpgsql'
-as $$
-    BEGIN
-        if u_count > 0 THEN
-            update products set count = count - u_count where id = u_id;
-        end if;
-        if tax > 0 THEN
-            update products set price = price + price * tax;
-        end if;
-    END;
-$$;
+call delete_data();
 
-create or replace function f_insert_data(i_name varchar, prod varchar, i_count integer, i_price integer)
+create or replace function delete_data_func()
 returns void
 language 'plpgsql'
 as
 $$
     begin
-        insert into products (name, producer, count, price)
-        values (i_name, prod, i_count, i_price);
+        delete from products where price < 41;
     end;
 $$;
 
-create or replace function f_update_data(u_count integer, tax float, u_id integer)
-returns integer
-language 'plpgsql'
-as
-$$
-    declare
-        result integer;
-    begin
-        if u_count > 0 THEN
-            update products set count = count - u_count where id = u_id;
-            select into result count from products where id = u_id;
-        end if;
-        if tax > 0 THEN
-            update products set price = price + price * tax;
-            select into result sum(price) from products;
-        end if;
-        return result;
-    end;
-$$;
+select delete_data_func();
