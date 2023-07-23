f47a912cb97bc728c7c73c4b298048e866004a13
Vetoshkin-Oleg <voa2014@gmail.com>
2023-07-23 17:41:51 +0500

    0. Принцип подстановки Лисков. [#4915  [#4915]]

---
+++ src/main/java/ru/job4j/ood/lsp/SomeOperatorSubscriber.java
@@ -0,0 +1,15 @@
+package ru.job4j.ood.lsp;
+
+class SomeOperatorSubscriber extends Subscriber {
+
+    public SomeOperatorSubscriber(PhoneNumber phoneNumber) {
+        super(phoneNumber);
+    }
+
+    @Override
+    public void setPhoneNumber(PhoneNumber phoneNumber) {
+        /* some specific logic; */
+        /* Забыли сделать проверку. Возможно не валидное состояние */
+        this.phoneNumber = phoneNumber;
+    }
+}
