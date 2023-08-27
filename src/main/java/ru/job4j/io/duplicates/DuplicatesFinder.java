0cdc839f09adafa603b169267053e17f68934539
Vetoshkin-Oleg <voa2014@gmail.com>
2023-08-27 18:01:43 +0500

    11. Поиск дубликатов [#315066 #440121]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,8 +6,8 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
         DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
-        System.out.println(duplicatesVisitor.getResult());
+        Files.walkFileTree(Path.of("./"), duplicatesVisitor);
+        duplicatesVisitor.print();
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -8,36 +8,29 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private final List<FileProperty> list = new ArrayList<>();
-    private final Set<FileProperty> set = new HashSet<>();
-    private final Set<String> result = new HashSet<>();
-    private final Map<Path, FileProperty> map = new HashMap<>();
+    private final Map<FileProperty, List<Path>> map = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(attrs.size(), file.getFileName().toString());
-        list.add(fileProperty);
-        map.put(file.toAbsolutePath().normalize(), fileProperty);
-
-        for (FileProperty fileProperty1 : list) {
-            if (Collections.frequency(list, fileProperty1) > 1) {
-                set.add(fileProperty1);
-            }
+        if (!map.containsKey(fileProperty)) {
+            List<Path> paths = new ArrayList<>();
+            paths.add(file.toAbsolutePath().normalize());
+            map.put(fileProperty, paths);
+        } else {
+            map.get(fileProperty).add(file.toAbsolutePath().normalize());
         }
-
-        for (Map.Entry<Path, FileProperty> entry : map.entrySet()) {
-            for (FileProperty f : set) {
-                if (f.equals(entry.getValue())) {
-                    System.out.println((entry.getKey().toString()));
-                    result.add(entry.getKey().toString());
-                }
-            }
-        }
-
         return super.visitFile(file, attrs);
     }
 
-    public Set<String> getResult() {
-        return result;
+    public void print() {
+        for (Map.Entry<FileProperty, List<Path>> entry : map.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                System.out.println(entry.getKey().getName() + " - " + entry.getKey().getSize() + " bytes");
+                for (Path p : entry.getValue()) {
+                    System.out.println(p.toAbsolutePath().normalize());
+                }
+            }
+        }
     }
 }
