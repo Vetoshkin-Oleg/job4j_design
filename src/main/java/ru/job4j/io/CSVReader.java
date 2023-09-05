d25be89e68f79917d1cd31710d3bb4749c5f9476
Vetoshkin-Oleg <voa2014@gmail.com>
2023-09-05 19:52:46 +0500

    17. Scanner [#504791 #441916]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -6,26 +6,21 @@ import java.nio.file.Path;
 import java.util.*;
 
 public class CSVReader {
-    private static Path sourceFile;
-    private static String delimiter;
-    private static String out;
-    private static String filter;
-    private static final List<String> SELECT = new ArrayList<>();
-    private static List<String> columns = new ArrayList<>();
 
     public static void handle(ArgsName argsName) throws IOException {
-        sourceFile = Path.of(argsName.get("path"));
-        delimiter = argsName.get("delimiter");
-        out = argsName.get("out");
-        filter = argsName.get("filter");
-        checkArguments();
-        filterColumn(sourceFile);
+        Path sourceFile = Path.of(argsName.get("path"));
+        String delimiter = argsName.get("delimiter");
+        String out = argsName.get("out");
+        String filter = argsName.get("filter");
+        checkArguments(argsName);
+        final List<String> select = filterColumn(sourceFile, delimiter, filter);
+        List<String> columns = List.of((readFile(sourceFile).get(0)).split(delimiter));
         List<String> content = readFile(sourceFile);
 
         StringBuilder stringBuilder = new StringBuilder();
         for (String s : content) {
             String[] temp = s.split(delimiter);
-            for (String selectedColumn : SELECT) {
+            for (String selectedColumn : select) {
                 int index = columns.indexOf(selectedColumn);
                 stringBuilder.append(temp[index]);
                 stringBuilder.append(delimiter);
@@ -36,15 +31,18 @@ public class CSVReader {
         print(stringBuilder.toString(), out);
     }
 
-    private static void filterColumn(Path path) throws IOException {
-        var scanner = new Scanner(path);
-        String headLine = scanner.nextLine();
-        columns = List.of(headLine.split(delimiter));
-        SELECT.addAll(Arrays.asList(filter.split(",")));
-        if (!new HashSet<>(columns).containsAll(SELECT)) {
-            throw new IllegalArgumentException("Четвертый аргумент командной строки введен некорректно: "
-                    + "не все значения параметра \"filter\" входят в список заголовков всех столбцов файла");
+    private static List<String> filterColumn(Path path, String delimiter, String filter) throws IOException {
+        List<String> select;
+        try (var scanner = new Scanner(path)) {
+            String headLine = scanner.nextLine();
+            List<String> columns = List.of(headLine.split(delimiter));
+            select = new ArrayList<>(Arrays.asList(filter.split(",")));
+            if (!new HashSet<>(columns).containsAll(select)) {
+                throw new IllegalArgumentException("Четвертый аргумент командной строки введен некорректно: "
+                        + "не все значения параметра \"filter\" входят в список заголовков всех столбцов файла");
+            }
         }
+        return select;
     }
 
     private static void print(String data, String out) {
@@ -61,10 +59,11 @@ public class CSVReader {
 
     public static List<String> readFile(Path path) throws IOException {
         List<String> result = new ArrayList<>();
-        var scanner = new Scanner(path);
-        while (scanner.hasNextLine()) {
-            String string = scanner.nextLine();
-            result.add(string);
+        try (var scanner = new Scanner(path)) {
+            while (scanner.hasNextLine()) {
+                String string = scanner.nextLine();
+                result.add(string);
+            }
         }
         return result;
     }
@@ -77,7 +76,9 @@ public class CSVReader {
         handle(argsName);
     }
 
-    private static void checkArguments() {
+    private static void checkArguments(ArgsName argsName) {
+        Path sourceFile = Path.of(argsName.get("path"));
+        String out = argsName.get("out");
         if (!(Files.isRegularFile(sourceFile)
                 && sourceFile.toString().endsWith(".csv"))) {
             throw new IllegalArgumentException("Первый аргумент командной строки не указывает на существующий файл с расширением \"csv\"");
