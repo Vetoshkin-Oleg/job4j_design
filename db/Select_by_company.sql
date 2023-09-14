6c72efd053e9fa10ad8f090b71c505a937b22c40
Vetoshkin-Oleg <voa2014@gmail.com>
2023-09-14 20:33:30 +0500

    3. SQL Select by company [#2760]

--- db/Select_by_company.sql
+++ db/Select_by_company.sql
@@ -1,3 +1,6 @@
+drop TABLE company;
+drop TABLE person;
+
 CREATE TABLE company(
     id integer NOT NULL,
     name character varying,
@@ -21,21 +24,21 @@ INSERT INTO company(id, name) VALUES (4, 'company_4');
 INSERT INTO company(id, name) VALUES (5, 'company_5');
 INSERT INTO company(id, name) VALUES (6, 'company_6');
 
-INSERT INTO person(id, name, company_id) VALUES (1, 'person_1', 1);
-INSERT INTO person(id, name, company_id) VALUES (2, 'person_2', 2);
-INSERT INTO person(id, name, company_id) VALUES (3, 'person_3', 3);
-INSERT INTO person(id, name, company_id) VALUES (4, 'person_4', 3);
-INSERT INTO person(id, name, company_id) VALUES (5, 'person_5', 4);
-INSERT INTO person(id, name, company_id) VALUES (6, 'person_6', 4);
-INSERT INTO person(id, name, company_id) VALUES (7, 'person_7', 4);
-INSERT INTO person(id, name, company_id) VALUES (8, 'person_8', 5);
-INSERT INTO person(id, name, company_id) VALUES (9, 'person_9', 5);
-INSERT INTO person(id, name, company_id) VALUES (10, 'person_10', 5);
-INSERT INTO person(id, name, company_id) VALUES (11, 'person_11', 5);
-INSERT INTO person(id, name, company_id) VALUES (12, 'person_12', 6);
-INSERT INTO person(id, name, company_id) VALUES (13, 'person_13', 6);
-INSERT INTO person(id, name, company_id) VALUES (14, 'person_14', 6);
-INSERT INTO person(id, name, company_id) VALUES (15, 'person_15', 6);
+INSERT INTO person(id, name, company_id) VALUES (1, 'person_1', 6);
+INSERT INTO person(id, name, company_id) VALUES (2, 'person_2', 5);
+INSERT INTO person(id, name, company_id) VALUES (3, 'person_3', 4);
+INSERT INTO person(id, name, company_id) VALUES (4, 'person_4', 4);
+INSERT INTO person(id, name, company_id) VALUES (5, 'person_5', 3);
+INSERT INTO person(id, name, company_id) VALUES (6, 'person_6', 3);
+INSERT INTO person(id, name, company_id) VALUES (7, 'person_7', 3);
+INSERT INTO person(id, name, company_id) VALUES (8, 'person_8', 2);
+INSERT INTO person(id, name, company_id) VALUES (9, 'person_9', 2);
+INSERT INTO person(id, name, company_id) VALUES (10, 'person_10', 2);
+INSERT INTO person(id, name, company_id) VALUES (11, 'person_11', 2);
+INSERT INTO person(id, name, company_id) VALUES (12, 'person_12', 1);
+INSERT INTO person(id, name, company_id) VALUES (13, 'person_13', 1);
+INSERT INTO person(id, name, company_id) VALUES (14, 'person_14', 1);
+INSERT INTO person(id, name, company_id) VALUES (15, 'person_15', 1);
 
 /*1. В одном запросе получить
 - имена всех person, которые не состоят в компании с id = 5;
@@ -48,3 +51,13 @@ where company.id != 5;
 /*2. Необходимо выбрать название компании с максимальным количеством человек
 + количество человек в этой компании.
 Нужно учесть, что таких компаний может быть несколько.*/
+select company.name, count(person.name)
+from person join company
+on person.company_id = company.id
+GROUP BY company.name
+having count(person.name) = (SELECT count(person.name)
+from person join company
+on person.company_id = company.id
+GROUP BY company.name
+order by count(person.name) desc
+limit 1)
