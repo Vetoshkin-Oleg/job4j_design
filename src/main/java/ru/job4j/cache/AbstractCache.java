ca0645f03bf6e8966a8dd431f11bf53e14334c19
Vetoshkin-Oleg <voa2014@gmail.com>
2023-11-01 23:57:05 +0800

    1. Реализация кеша на SoftReference [#1592 #455755]

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -15,12 +15,14 @@ public abstract class AbstractCache<K, V> {
 
     public final V get(K key) throws IOException {
         SoftReference<V> softReference = cache.get(key);
-        V result;
-        if (softReference == null) {
-            result = load(key);
-            put(key, result);
-        } else {
-            result = softReference.get();
+        V result = null;
+        if (key != null) {
+            if (softReference == null) {
+                result = load(key);
+                put(key, result);
+            } else {
+                result = softReference.get();
+            }
         }
         return result;
     }
