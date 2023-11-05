d13039846dd0ef237c3a092d2af5fcb3cc3cead1
Vetoshkin-Oleg <voa2014@gmail.com>
2023-11-05 23:22:57 +0800

    1. Отчеты. [#850]

--- src/main/java/ru/job4j/ood/srp/currency/InMemoryCurrencyConverter.java
+++ src/main/java/ru/job4j/ood/srp/currency/InMemoryCurrencyConverter.java
@@ -5,12 +5,12 @@ public class InMemoryCurrencyConverter implements CurrencyConverter {
     private final double[][] conversationTable = new double[CURRENCIES_COUNT][CURRENCIES_COUNT];
 
     public InMemoryCurrencyConverter() {
-        this.conversationTable[Currency.RUB.ordinal()][Currency.USD.ordinal()] = 0.0162;
-        this.conversationTable[Currency.RUB.ordinal()][Currency.EUR.ordinal()] = 0.0166;
-        this.conversationTable[Currency.USD.ordinal()][Currency.EUR.ordinal()] = 1.0218;
-        this.conversationTable[Currency.USD.ordinal()][Currency.RUB.ordinal()] = 65D;
-        this.conversationTable[Currency.EUR.ordinal()][Currency.USD.ordinal()] = 0.9786;
-        this.conversationTable[Currency.EUR.ordinal()][Currency.RUB.ordinal()] = 63D;
+        this.conversationTable[Currency.RUB.ordinal()][Currency.USD.ordinal()] = 0.0109D;
+        this.conversationTable[Currency.RUB.ordinal()][Currency.EUR.ordinal()] = 0.0102D;
+        this.conversationTable[Currency.USD.ordinal()][Currency.EUR.ordinal()] = 0.9346D;
+        this.conversationTable[Currency.USD.ordinal()][Currency.RUB.ordinal()] = 91.73D;
+        this.conversationTable[Currency.EUR.ordinal()][Currency.USD.ordinal()] = 1.07D;
+        this.conversationTable[Currency.EUR.ordinal()][Currency.RUB.ordinal()] = 98.49D;
     }
 
     @Override
---
+++ src/test/java/ru/job4j/ood/srp/report/ReportAccountingTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.ood.srp.report;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.srp.currency.Currency;
+import ru.job4j.ood.srp.currency.InMemoryCurrencyConverter;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.MemStore;
+
+import java.util.Calendar;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ReportAccountingTest {
+
+    @Test
+    void generateAccountingEuroToRub() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100D);
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        store.add(worker);
+        Report engine = new ReportAccounting(store, parser, new InMemoryCurrencyConverter(),
+                Currency.EUR, Currency.RUB);
+        String expect = "Name; Hired; Fired; Salary;"
+                + System.lineSeparator()
+                + worker.getName() + " "
+                + parser.parse(worker.getHired()) + " "
+                + parser.parse(worker.getFired()) + " "
+                + "9849.0"
+                + System.lineSeparator();
+        assertThat(engine.generate(em -> true)).isEqualTo(expect);
+    }
+}
