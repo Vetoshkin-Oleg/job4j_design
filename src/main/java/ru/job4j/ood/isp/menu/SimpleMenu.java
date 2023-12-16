36c0fb94098933c598fab844ec59cd22d65b90cf
Vetoshkin-Oleg <voa2014@gmail.com>
2023-12-16 22:06:47 +0800

    1. Создать меню. [#4748]

---
+++ src/main/java/ru/job4j/ood/isp/menu/SimpleMenu.java
@@ -0,0 +1,103 @@
+package ru.job4j.ood.isp.menu;
+
+import java.util.*;
+
+public class SimpleMenu implements Menu {
+
+    private final List<MenuItem> rootElements = new ArrayList<>();
+
+    @Override
+    public boolean add(String parentName, String childName, ActionDelegate actionDelegate) {
+        return false;
+    }
+
+    @Override
+    public Optional<MenuItemInfo> select(String itemName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Iterator<MenuItemInfo> iterator() {
+        return null;
+    }
+
+    private Optional<ItemInfo> findItem(String name) {
+        return Optional.empty();
+    }
+
+    private static class SimpleMenuItem implements MenuItem {
+
+        private final String name;
+        private final List<MenuItem> children = new ArrayList<>();
+        private final ActionDelegate actionDelegate;
+
+        public SimpleMenuItem(String name, ActionDelegate actionDelegate) {
+            this.name = name;
+            this.actionDelegate = actionDelegate;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public List<MenuItem> getChildren() {
+            return children;
+        }
+
+        @Override
+        public ActionDelegate getActionDelegate() {
+            return actionDelegate;
+        }
+    }
+
+    private class DFSIterator implements Iterator<ItemInfo> {
+
+        Deque<MenuItem> stack = new LinkedList<>();
+
+        Deque<String> numbers = new LinkedList<>();
+
+        DFSIterator() {
+            int number = 1;
+            for (MenuItem item : rootElements) {
+                stack.addLast(item);
+                numbers.addLast(String.valueOf(number++).concat("."));
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public ItemInfo next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            MenuItem current = stack.removeFirst();
+            String lastNumber = numbers.removeFirst();
+            List<MenuItem> children = current.getChildren();
+            int currentNumber = children.size();
+            for (var i = children.listIterator(children.size()); i.hasPrevious();) {
+                stack.addFirst(i.previous());
+                numbers.addFirst(lastNumber.concat(String.valueOf(currentNumber--)).concat("."));
+            }
+            return new ItemInfo(current, lastNumber);
+        }
+
+    }
+
+    private static class ItemInfo {
+
+        MenuItem menuItem;
+        String number;
+
+        public ItemInfo(MenuItem menuItem, String number) {
+            this.menuItem = menuItem;
+            this.number = number;
+        }
+    }
+
+}
